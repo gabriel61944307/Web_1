22d9ede268a51fc50d0de7f40094f19c71ab1768
Tiago Lucas <tiagolucasleal570@gmail.com>
2023-09-03 21:13:50 -0300

    consultas por medico e paciente ok

--- consultorio-T3/consultorio/src/main/java/br/ufscar/dc/dsw/controller/ConsultaRestController.java
+++ consultorio-T3/consultorio/src/main/java/br/ufscar/dc/dsw/controller/ConsultaRestController.java
@@ -1,8 +1,10 @@
 package br.ufscar.dc.dsw.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.ufscar.dc.dsw.domain.Consulta;
+import br.ufscar.dc.dsw.domain.Medico;
+import br.ufscar.dc.dsw.domain.Paciente;
 import br.ufscar.dc.dsw.service.spec.IConsultaService;
+import br.ufscar.dc.dsw.service.spec.IMedicoService;
+import br.ufscar.dc.dsw.service.spec.IPacienteService;
 
 @RestController
 public class ConsultaRestController {
     
     @Autowired
     private IConsultaService service;
+    
+    @Autowired
+    private IPacienteService servicePaciente;
+
+    @Autowired
+    private IMedicoService serviceMedico;
 
     @Autowired
     BCryptPasswordEncoder encoder;
@@ -50,6 +62,26 @@ public class ConsultaRestController {
 	public ResponseEntity<Consulta> lista(@PathVariable("id") long id) {
         Consulta consulta = service.buscarPorId(id);
 		if (consulta == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(consulta);
+	}
+
+    @GetMapping(path = "/consultas/pacientes/{id}")
+	public ResponseEntity<List<Consulta>> listaPacientes(@PathVariable("id") long id) {
+        Paciente paciente = servicePaciente.buscarPorId(id);
+        List<Consulta> consulta = service.buscarTodasPorPaciente(paciente);
+		if (consulta.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(consulta);
+	}
+
+    @GetMapping(path = "/consultas/medicos/{id}")
+	public ResponseEntity<List<Consulta>> listaMedicos(@PathVariable("id") long id) {
+        Medico medico = serviceMedico.buscarPorId(id);
+        List<Consulta> consulta = service.buscarTodasPorMedico(medico);
+		if (consulta.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(consulta);
